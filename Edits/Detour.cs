1e15906253373b614e9e71cc062d3623ef609984
Destructor_Ben <ben.honeyfield@gmail.com>
2023-08-20 16:44:14 +1200

    Initial work for ILEdit and Detour abstraction

---
+++ Edits/Detour.cs
@@ -0,0 +1,13 @@
+﻿namespace TerraUtil.Edits;
+public abstract class Detour : TerraUtilModType
+{
+    protected sealed override void Register() { }
+
+    public sealed override void SetupContent()
+    {
+        Apply();
+        SetStaticDefaults();
+    }
+
+    public abstract void Apply();
+}
---
+++ Edits/ILEdit.cs
@@ -0,0 +1,46 @@
+﻿using MonoMod.Cil;
+using System.Reflection;
+
+namespace TerraUtil.Edits;
+/// <summary>
+/// An abstraction of an ILEdit, packaged into a ModType.
+/// </summary>
+public abstract class ILEdit : TerraUtilModType
+{
+    protected sealed override void Register() { }
+
+    public sealed override void SetupContent()
+    {
+        Setup();
+        SetStaticDefaults();
+    }
+
+    public abstract void Setup();
+    public abstract void Apply(ILCursor c);
+
+    public void Edit(ILContext il)
+    {
+        try
+        {
+            var c = new ILCursor(il);
+            Apply(c);
+        }
+        catch (Exception e)
+        {
+            throw new ILPatchFailureException(Mod, il, e);
+        }
+    }
+}
+
+/// <summary>
+/// An abstraction of an ILEdit that uses reflection, packaged into a ModType.
+/// </summary>
+public abstract class ILEditReflection : ILEdit
+{
+    public abstract MethodInfo Method { get; }
+
+    public override void Setup()
+    {
+        MonoModHooks.Modify(Method, Edit);
+    }
+}
